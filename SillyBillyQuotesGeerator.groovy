a8233bf91d62afbfedec6437ef9319376ee6d1d2
AnastasiaBlack <planet.of.translation@gmail.com>
2017-09-05 12:53:07 +0300

    Edited a quotes generator so that it writes the geerated quotes to the db.

--- SillyBillyQuotesGeerator.groovy
+++ SillyBillyQuotesGeerator.groovy
@@ -1,3 +1,7 @@
+import myquote.*
+
+//println Quote.count()
+
 def authors = ["Mannie Nock", "Goodween Barrow"]
 def quoteParts = [["Time ", "waits for ", "no man"], 
 ["The Wolf ", "eats ", "meat"],
@@ -9,11 +13,14 @@ String createQuote(List quoteParts, List authors){
 def random = new Random()
 def m = quoteParts.size()
 def n = authors.size()
-
+def quote = quoteParts[random.nextInt(m)][0]+" " + quoteParts[random.nextInt(m)][1]+" " + quoteParts[random.nextInt(m)][2]
+def author = authors[random.nextInt(n)]
+new Quote(author: author, content: quote).save()
 return quoteParts[random.nextInt(m)][0]+" " + quoteParts[random.nextInt(m)][1]+" " + quoteParts[random.nextInt(m)][2]+" " + authors[random.nextInt(n)]
 }
 
 for(int i in 0..<10){
 def quote = createQuote(quoteParts, authors)
+
 println quote
 }
